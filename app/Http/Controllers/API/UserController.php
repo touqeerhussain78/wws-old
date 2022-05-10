c0fa958eb282ec5b69accc63352b5f59f0a23d5f
Arsalan Raja <touqeerhussain78>
2022-05-10 08:19:28 +0000

    first commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Register
+     */
+    public function register(Request $request)
+    {
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+
+            $token = $user->createToken('API Token')->plainTextToken;
+
+            $success = true;
+            $message = 'User register successfully';
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+
+        // response
+        $response = [
+            'success' => $success,
+            'token' => $token,
+            'message' => $message,
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (Auth::attempt($credentials, true)) {
+            $success = true;
+
+            $token  = auth()->user()->createToken('API Token')->plainTextToken;
+            $message = 'User login successfully';
+        } else {
+            $success = false;
+            $message = 'Unauthorised';
+        }
+
+        // response
+        $response = [
+            'token' => $token,
+            'success' => $success,
+            'message' => $message,
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Logout
+     */
+    public function logout()
+    {
+
+
+        
+        try {
+
+            $id = Auth::user()->id;
+            User::where('id',$id)->update(['remember_token'=>NULL]);
+
+            // $request->user()->currentAccessToken()->delete();
+
+
+
+            Session::flush();
+            $success = true;
+            $message = 'Successfully logged out';
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $success = false;
+            $message = $ex->getMessage();
+        }
+
+        // response
+        $response = [
+            'success' => $success,
+            'message' => $message,
+        ];
+        return response()->json($response);
+    }
+}
